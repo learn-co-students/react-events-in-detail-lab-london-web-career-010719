38218cf87cc24ce8f288b366a8fa111b3a0fbeae
Parik Panchal <flatiron@Flatirons-MacBook-Air-4.local>
2019-03-15 10:27:47 +0000

    Done.

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,18 @@
-// Code CoordinatesButton Component Here
+import React, {Component} from 'react'
+
+export default class CoordinatesButton extends Component {
+
+
+    handleClick = (event) => {
+        let arr = []
+        arr[0] = event.clientX
+        arr[1] = event.clientY
+        this.props.onReceiveCoordinates(arr)
+    };
+
+    render() {
+        return(
+            <button onClick={this.handleClick}></button>
+        );
+    };
+};
--- src/index.js
+++ src/index.js
@@ -4,11 +4,14 @@ import ReactDOM from 'react-dom';
 // These must be defined for code to work in browser
 import CoordinatesButton from './components/CoordinatesButton';
 import DelayedButton from './components/DelayedButton';
+import Test from './components/TestComponent';
 
 ReactDOM.render(
   <div>
-    <CoordinatesButton onReceiveCoordinates={ mouseCoordinates => console.log(mouseCoordinates) } />
+    <CoordinatesButton onReceiveCoordinates={ mouseCoordinates => console.log(mouseCoordinates)}/>
     <DelayedButton onDelayedClick={ event => console.log(event) } delay={1500} />
+    <br></br>
+    <Test />
   </div>,
   document.getElementById('global')
 );
